b7723f4ed070827cb4c3376eba78f652ff9728d5
Seth Addo <59029978+seths10@users.noreply.github.com>
2021-05-26 19:22:37 +0000

    Create linear_search.cpp
    This is a linear search implementation algorithm

---
+++ Searching Algorithms/linear_search.cpp
@@ -0,0 +1,32 @@
+//This is the implementation of a linear search algorithm
+// ===========================================================================
+//Created by Seth Addo
+ 
+#include <iostream>
+
+using namespace std;
+
+int linear_search(int search[],int SIZE, int searchValue)
+{
+    for(int count=0;count<SIZE;count++)
+    {
+        if (search[count] == searchValue)
+        {
+            cout<<"Your number is at index "<< count<<endl;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int number;
+    const int SIZE = 10;
+    int numbers[SIZE] = {1,8,67,2,4,1,5,6,3,7};
+
+    cout<<"Enter the number you are searching for "<<endl;
+    cin>>number;
+
+    linear_search(numbers,10, number);
+}
+
